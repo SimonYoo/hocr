484e3c36a45f3d198fabba28a8bc2f71280032cb
cmonyo <cmonyo@cmonyo-PC>
2016-03-21 16:05:40 +0900

    Test git hub

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <stdio.h>
+#include "opencv2/ml/ml.hpp"
+#include<opencv2/ml/ml.hpp>
+#include "opencv/ml.h"
+#include "opencv/cv.h"
+#include "dcRecogNum.h"
+#include "dcOCRDiv.h"
+#include "dcDivision.h"
+#include<vector>
+#include<sstream>
+#include<fstream>
+#include <stdio.h>
+#include <ctype.h>
+#include <ctime>
+#include <dirent.h>
+
+using namespace cv;
+using namespace std;
+
+
